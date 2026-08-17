c0272ed0fab7866ba73c314d525840867a645e1c
Faizan Ali <Faizanalyhh@gmail.com>
2026-08-17 15:32:55 +0500

    feat: updated styling

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 def blog_list(request):
-    return render(request, 'list.html')
+    posts = Blog.objects.all()
+    carousel = Blog.objects.all().order_by('-id')[:4]
+    return render(request, 'list.html', {'posts': posts, 'carousel': carousel})
 
 def blog_detail(request):
     return render(request, 'detail.html')
